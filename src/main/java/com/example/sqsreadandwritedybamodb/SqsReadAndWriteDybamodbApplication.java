b70f4b19a581755cb7acdbf3eb59194f81457b30
maximzvyagin <Max198805@yandex.ru>
2021-09-17 13:07:22 +0300

    sqs read and write dynamodb

---
+++ src/main/java/com/example/sqsreadandwritedybamodb/SqsReadAndWriteDybamodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqsreadandwritedybamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsReadAndWriteDybamodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqsReadAndWriteDybamodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqsreadandwritedybamodb/SqsReadAndWriteDybamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqsreadandwritedybamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsReadAndWriteDybamodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
